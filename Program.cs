82e141904623416f5d04c47e4cfade12b4ccc640
dorukyilmaz143 <yilmazz.dorukk@gmail.com>
2025-05-06 20:14:32 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ilkuygulamam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Doruk Yılmaz");
+            Console.WriteLine("Üniversitede öğrendiklerim haricinde yazılım eğitimi alıp kendimi geliştirmek için bu Bootcampteyim.");
+        }
+    }
+}
